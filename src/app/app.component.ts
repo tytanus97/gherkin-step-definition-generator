85d1a8fbb9db09fb1f058770b820efdf71f2fa38
pawel.ataman <pawel.ataman@finanteq.com>
2023-01-01 18:00:38 +0100

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gherkin-step-definition-generator-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gherkin-step-definition-generator';
+}
