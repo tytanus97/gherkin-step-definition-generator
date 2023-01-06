4f8ae6026c14b645e7130f06914165a74ad0ac41
pawel.ataman <pawel.ataman@finanteq.com>
2023-01-06 21:49:11 +0100

    display feature file fix

--- src/app/start-screen/ui/feature-select/feature-select.component.ts
+++ src/app/start-screen/ui/feature-select/feature-select.component.ts
@@ -1,17 +1,22 @@
-import { featureFileContent$ } from './../../state/feature.selectors';
+import { featureFile$ } from './../../state/feature.selectors';
 import { FeatureState } from './../../state/feature.state';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { loadFeatureFile } from '../../state/feature.actions';
+import { map, Observable } from 'rxjs';
 
 @Component({
 	selector: 'gherkin-step-definition-generator-feature-select',
 	templateUrl: './feature-select.component.html',
 	styleUrls: ['./feature-select.component.scss'],
 })
-export class FeatureSelectComponent {
+export class FeatureSelectComponent implements OnInit {
 
+	chooseFileLabel$: Observable<string>
 	constructor(private readonly featureStore: Store<FeatureState>) { }
+	ngOnInit(): void {
+		this.chooseFileLabel$ = this.featureStore.select(featureFile$).pipe(map(file => !file ? 'Choose feature file' : 'Change file'))
+	}
 
 
 
