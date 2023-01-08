ac661d70db94696efccff2340e188688b2b53983
pawel.ataman <pawel.ataman@finanteq.com>
2023-01-08 19:05:18 +0100

    feature file name

--- src/app/start-screen/ui/feature-select/feature-select.component.ts
+++ src/app/start-screen/ui/feature-select/feature-select.component.ts
@@ -14,11 +14,13 @@ export class FeatureSelectComponent implements OnInit {
 
 	chooseFileLabel$: Observable<string>
 	featureTextContent$: Observable<string>
+	featureFileName$: Observable<string>
 
 	constructor(private readonly featureStore: Store<FeatureState>) { }
 	ngOnInit(): void {
 		this.chooseFileLabel$ = this.featureStore.select(featureFile$).pipe(map(file => !file ? 'Choose feature file' : 'Change file'))
 		this.featureTextContent$ = this.featureStore.select(featureFileContent$)
+		this.featureFileName$ = this.featureStore.select(featureFile$).pipe(map((file: File) => file?.name || ''))
 
 	}
 
